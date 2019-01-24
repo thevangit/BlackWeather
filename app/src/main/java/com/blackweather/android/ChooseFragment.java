65736c47e07f62dd69484728b1df9f8aabaec6ea
theVan <xmingm2333@gmail>
2019-01-24 16:51:53 +0800

    修复刷新定位的bug

--- app/src/main/java/com/blackweather/android/ChooseFragment.java
+++ app/src/main/java/com/blackweather/android/ChooseFragment.java
@@ -20,6 +20,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -137,6 +138,7 @@ public class ChooseFragment extends Fragment implements
         mRefreshLayout = view.findViewById(R.id.choose_refresh);
         mProgressBar.setVisibility(View.GONE);
         mCurrentLocationView.setVisibility(View.GONE);
+//        mRefreshLayout.setEnabled(false);
         // 处理运行时权限
         List<String> permissionList = checkPermissionList();
         if (!permissionList.isEmpty()) {
@@ -272,6 +274,23 @@ public class ChooseFragment extends Fragment implements
                 showCurrentLocation();
             }
         });
+
+//        mListView.setOnScrollListener(new AbsListView.OnScrollListener() {
+//            @Override
+//            public void onScrollStateChanged(AbsListView view, int scrollState) {
+//
+//            }
+//
+//            @Override
+//            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount,
+//                                 int totalItemCount) {
+//                if (firstVisibleItem == 0 && view.getTop() == 0){
+//                    mRefreshLayout.setEnabled(true);
+//                } else {
+//                    mRefreshLayout.setEnabled(false);
+//                }
+//            }
+//        });
 
         // 定位到的location
         mCurrentLocationView.setOnClickListener(new View.OnClickListener() {
